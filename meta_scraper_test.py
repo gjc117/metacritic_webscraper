df95d452e26435082b2c9f9c7b4f3acebb5ea0af
Gavin <gavinjcreagh@yahoo.co.uk>
2018-10-25 17:33:49 +0100

    functioning scraper for best of past 90 days

---
+++ meta_scraper_test.py
@@ -0,0 +1,46 @@
+import sys
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup as soup
+
+url = 'https://www.metacritic.com/browse/games/score/metascore/90day/all/filtered?sort=desc'
+
+req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+
+web_byte = urlopen(req).read()
+webpage = web_byte.decode('utf-8')
+urlopen(req).close()
+
+page_soup = soup(webpage, "html.parser")
+
+#classes of first and last are unique, and could not be contained within the main loop
+prod_f = page_soup.findAll("div", {"class":"product_row game first"})
+prods = page_soup.findAll("div", {"class":"product_row game"})
+prod_l = page_soup.findAll("div", {"class":"product_row game last"})
+
+
+prod_f.extend(prods)
+prod_f.extend(prod_l)
+
+filename = "games.csv"
+f = open(filename, "w")
+
+headers = "rank, title, score, release_date\n"
+f.write(headers)
+
+for prod in prod_f:
+
+	rank = prod.select('div')[0].get_text(strip=True)
+	title = prod.select('div')[3].get_text(strip=True).replace('\n',' ')
+
+	score = prod.select('div')[2].get_text(strip=True)
+
+	release = prod.select('div')[5].get_text(strip=True)
+
+	print("rank: " + rank)
+	print("title: " + title)
+	print("score: " + score)
+	print("release: " + release +"\n\n")
+
+	f.write(rank + "," + title + "," + score + "," + release.replace(",","|") + "\n")
+
+f.close()
